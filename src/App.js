3184d5e9451bf54169caea8d1bc9cb7522acfd4b
Matias <57120079+elgrantut@users.noreply.github.com>
2020-10-11 17:17:21 -0300

    Data fetching from a button

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './styles.css'
 
 export default function App() {
   return (
-    <div className="container">
+    <div className="container m-5">
       <h1 className="text-primary display-4">Hello World</h1>
       <DataFetching />
     </div>
--- src/components/DataFetching.jsx
+++ src/components/DataFetching.jsx
@@ -4,8 +4,12 @@ import React, { useEffect, useState } from 'react'
 export default function DataFetching() {
   const [post, setPost] = useState({})
   const [id, setId] = useState(1)
+  const [idFromButtonClick, setIdFromButtonClick] = useState(1)
+  const handleClick = () => {
+    setIdFromButtonClick(id)
+  }
   useEffect(() => {
-    Axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+    Axios.get(`https://jsonplaceholder.typicode.com/posts/${idFromButtonClick}`)
       .then((res) => {
         console.log(res)
         setPost(res.data)
@@ -13,12 +17,20 @@ export default function DataFetching() {
       .catch((err) => {
         console.log(err)
       })
-  }, [id])
+  }, [idFromButtonClick])
 
   return (
     <div>
-      <input type="text" value={id} onChange={(e) => setId(e.target.value)} />
       <h6 className="text-primary my-3">{post.title}</h6>
+      <input
+        className="form-control"
+        type="text"
+        value={id}
+        onChange={(e) => setId(e.target.value)}
+      />
+      <button className="btn btn-primary my-2" onClick={handleClick}>
+        Fetch Post
+      </button>
     </div>
   )
 }
